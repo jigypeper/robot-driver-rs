3f704af09358b01375d261ba6324b0846c7ccf65
Ahmed Hagi <ahmedosman@mac.com>
2025-01-25 15:25:24 +0000

    working on robot functionality

--- src/mockrobot/robot.rs
+++ src/mockrobot/robot.rs
@@ -4,19 +4,33 @@ pub enum State {
     Ready,
     InProgress,
     Complete,
-    Error,
+    Error, // need an error enum here
 }
 
 pub struct Robot {
     pub id: String,
     pub state: State,
+    pub location: Location,
 }
 
+pub struct Location(i32, i32, i32);
+
 impl Robot {
-    fn new() -> Self {
+    pub fn new() -> Self {
         Robot {
             id: Uuid::new_v4().to_string(),
             state: State::Ready,
+            location: Location(0, 0, 0),
         }
+    }
+
+    pub fn home(&mut self) -> Self {
+        todo!();
+    }
+    pub fn pick(&mut self, location: Location) -> Self {
+        todo!();
+    }
+    pub fn place(&mut self, location: Location) -> Self {
+        todo!();
     }
 }
